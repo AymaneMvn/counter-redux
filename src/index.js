e12cfad3ea37469635f93681a97e86e336f0cb65
AYMANE JEDDAD <mmvn02@gmail.com>
2023-07-02 01:16:28 +0100

    finale-commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from '././Components/store';
-import Api from './Components/API/api';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
